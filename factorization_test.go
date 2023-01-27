53c15858e07d6d942e9bee5607ed92261446fd59
CC11001100 <CC11001100@qq.com>
2023-01-27 15:32:43 +0800

    引入素数表避免无效计算以提升寻找速度

--- factorization_test.go
+++ factorization_test.go
@@ -3,9 +3,13 @@ package factorization
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestFactorization(t *testing.T) {
-	factorization := Factorization(99999999)
-	fmt.Println(factorization)
+	beginTime := time.Now()
+	factorization := Factorization(uint64(1231244681123))
+	cost := time.Now().Sub(beginTime)
+	fmt.Println(factorization) // Output: [191749 6421127]
+	fmt.Println(cost)          // Output: 9.1188ms
 }
