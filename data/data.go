546bf15a6b9ef1f6290cdeda69a12475402bfa5c
CC11001100 <CC11001100@qq.com>
2024-09-03 23:03:10 +0800

    feat: 增加注释

--- data/data.go
+++ data/data.go
@@ -2,5 +2,7 @@ package data
 
 import _ "embed"
 
+// PrimeNumberTable 打包时内嵌的素数表，把前几万个素数都放进来加速因式分解的过程
+//
 //go:embed prime-number-table.txt
 var PrimeNumberTable string
--- prime-number-table.go
+++ prime-number-table.go
@@ -8,11 +8,17 @@ import (
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+// PrimeNumberTable 素数表，用于封装查表相关的一些操作
 type PrimeNumberTable struct {
+
+	// 底层的素数表
 	primeNumberTable []uint64
-	max              uint64
+
+	// 当前的素数表中最大的数字，用于在使用判断数字是否超出了素数表的范畴
+	max uint64
 }
 
+// NewPrimeNumberTable 创建一张素数表
 func NewPrimeNumberTable() *PrimeNumberTable {
 	return &PrimeNumberTable{
 		primeNumberTable: make([]uint64, 0),
@@ -20,6 +26,8 @@ func NewPrimeNumberTable() *PrimeNumberTable {
 	}
 }
 
+// Init 使用给定的素数表初始化，lines是每行一个素数，并且是从小到大排好序的
+// 这个方法可以重复调用，每次重复调用的时候会把之后的素数表完全刷新掉
 func (x *PrimeNumberTable) Init(lines string) error {
 	x.primeNumberTable = make([]uint64, 0)
 	split := strings.Split(lines, "\n")
@@ -44,14 +52,19 @@ func (x *PrimeNumberTable) Init(lines string) error {
 	return nil
 }
 
-// findFirstFactorByEnd 找到第一个因子
+// FindFirstFactor 找到n的最小的一个因子
+// return:
+//
+//	firstFactor: 找到的n的第一个因子
+//	isFindSuccess: 是否找到了素数，仅在此参数为true的情况下firstFactor的值才有效
+//	isCanFind: 当前素数表是否能够起到加速的作用，如果不能起到加速作用的话直接返回false，仅在此参数为true的前提下firstFactor和isFindSuccess才有效
 func (x *PrimeNumberTable) FindFirstFactor(n uint64) (firstFactor uint64, isFindSuccess bool, isCanFind bool) {
 
 	if n > x.max {
 		return 0, false, false
 	}
 
-	// 跳着往前寻找约数，性能有明显上升
+	// 按照素数表跳着往后寻找约数，性能有明显上升，越往后性能加速越快，因为省略掉了无效数字的取余运算
 	for _, i := range x.primeNumberTable {
 		if n%i == 0 {
 			return i, true, true
@@ -63,8 +76,10 @@ func (x *PrimeNumberTable) FindFirstFactor(n uint64) (firstFactor uint64, isFind
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+// DefaultPrimeNumberTable 默认使用的素数表，如果觉得内置的素数表太小的话，可以使用自定义的素数表替换掉默认的素数表
 var DefaultPrimeNumberTable *PrimeNumberTable
 
+// 初始化默认的素数表
 func init() {
 	DefaultPrimeNumberTable = NewPrimeNumberTable()
 	err := DefaultPrimeNumberTable.Init(data.PrimeNumberTable)
